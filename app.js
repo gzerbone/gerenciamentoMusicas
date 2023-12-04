8fc06f595a3c3c54074e2cd27a8e6430dc798c5e
Gabriela Zerbone <gzmbaptista.cic@uesc.br>
2023-12-04 00:30:49 -0300

    inserção do album funcionando

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
   //Pondo informações do cards
   const query = 
   `
-  SELECT album.id_album ,musica.titulo, album.ano_lancamento, album.titulo_album, artista.nome , artista.id_artista
+  SELECT album.id_album ,musica.titulo, musica.id_musica, album.ano_lancamento, album.titulo_album, artista.nome , artista.id_artista
   FROM musica 
   INNER JOIN album ON musica.id_album = album.id_album
   INNER JOIN artista ON album.id_artista = artista.id_artista 
@@ -31,23 +31,9 @@ app.get('/', (req, res) => {
   INSERÇÃO
 ********/
 
-app.post('/salvar-album', (req, res) => {
-  const { id_artista, titulo_album } = req.body;
-
-  const query = 'INSERT INTO album (titulo_album, id_artista) VALUES (?, ?)';
-  mysql.query(query, [titulo_album, id_artista], (err, result) => {
-    if (err) {
-      console.error('Erro ao inserir álbum no banco de dados:', err);
-    } else {
-      console.log('Álbum inserido com sucesso');
-    }
-    
-  });
-});
-
 // (2)GET /inserir
 app.get('/inserir', (req, res) => {
-  const query = 'SELECT id_artista, nome FROM artista';
+  const query = 'SELECT id_artista, nome  FROM artista';
 
   mysql.query(query, (err, results) => {
     if (err) {
@@ -62,28 +48,31 @@ app.get('/inserir', (req, res) => {
 
   
   // (2)POST /inserir
-app.post("/inserir", function (req, res) {
-  //Inserir Artista
-  const insert_artista = "INSERT INTO artista (nome) VALUES (?)";
-  const dadosArtista = [req.body.nome];
-  mysql.query(insert_artista, dadosArtista, function(err) {});
-
+  app.post("/inserir", function (req, res) {
+    //Inserir Artista
+    const insert_artista = "INSERT INTO artista (nome) VALUES (?)";
+    const dadosArtista = [req.body.nome];
+    mysql.query(insert_artista, dadosArtista, function(err) {});
 
   //RECUPERAÇÃO DE ID DO ARTISTA PARA RELACIONAMENTO COM A TABELA ALBUM
   FK_artista = "";
   const ref_artista = "SELECT * FROM artista ORDER BY id_artista DESC limit 1";
   mysql.query(ref_artista, [], function(err, IdArtista){
   FK_artista = IdArtista[0].id_artista;
-    
+  console.log("artista", FK_artista);
+
 
   //Inserir Album
   const insert_album = "INSERT INTO album (titulo_album, ano_lancamento, id_artista) VALUES (?, ?, ?)";
   const dadosAlbum = [req.body.titulo_album, req.body.ano_lancamento, FK_artista];
+  //testando se possui valor titulo_album
+  console.log(req.body.titulo_album);
+
   mysql.query(insert_album, dadosAlbum, function(err) {});
   });
 
 
-  //RECUPERAÇÃO DE ID's PARA O RELACIONAMENTO
+  //RECUPERAÇÃO DO ID_ALBUM PARA O RELACIONAMENTO COM A TABELA MUSICA
   FK_album = "";
   const ref_album = "SELECT * FROM album ORDER BY id_album DESC limit 1";
   mysql.query(ref_album, [], function(err, IdAlbum){
@@ -92,8 +81,8 @@ app.post("/inserir", function (req, res) {
 
 
     //Inserir MÚSICA
-    const sql = "INSERT INTO musica (titulo, ano_lancamento, id_album) VALUES (?, ?, ?)";
-    const dadosMestre = [req.body.titulo, req.body.ano_lancamento, FK_album];
+    const sql = "INSERT INTO musica (titulo, id_album) VALUES (?, ?)";
+    const dadosMestre = [req.body.titulo, FK_album];
     mysql.query(sql, dadosMestre, function (err) {
       if (err) {
       return console.error(err.message);
@@ -103,26 +92,60 @@ app.post("/inserir", function (req, res) {
   });
 });
 
+app.post('/salvar-album', (req, res) => {
+  if(req.body.titulo_album === 'novoArtista'){
+    const { titulo_album, ano_lancamento, id_artista } = req.body;
+
+    const query = 'INSERT INTO album (titulo_album, ano_lancamento, id_artista) VALUES (?, ?, ?)';
+    mysql.query(query, [titulo_album, ano_lancamento, id_artista], (err, result) => {
+      if (err) {
+        console.error('Erro ao inserir álbum no banco de dados:', err);
+      } else {
+        console.log('Álbum inserido com sucesso');
+      }
+    });}    
+    const { id_artista, titulo_album } = req.body;
+
+    const query = 'INSERT INTO album (titulo_album, id_artista) VALUES (?, ?)';
+    mysql.query(query, [titulo_album, id_artista], (err, result) => {
+      if (err) {
+        console.error('Erro ao inserir álbum no banco de dados:', err);
+      } else {
+        console.log('Álbum inserido com sucesso');
+      }
+      
+    });
+});
 
 /*******
   UPDATE
 ********/
 
+// Busca os nome do albuns, ano de lanc. e os artistas armazenados no bd
 app.get('/editar-album/:id', (req, res) => {
   const albumId = req.params.id;
-  const query = 
-  `
-  SELECT album.id_album ,album.titulo_album, album.ano_lancamento, artista.id_artista, artista.nome
-  FROM album 
-  INNER JOIN artista ON album.id_artista = artista.id_artista
-  WHERE album.id_album = ?
-  `;
+
   // Consulta ao banco de dados para obter os detalhes do álbum a ser editado
-  mysql.query(query, [albumId], (error, dados) => {    
-    // Renderiza o formulário de edição e passa os detalhes do álbum para o template EJS
-    res.render('editar', {dados});
+  const queryAlbum = `
+      SELECT album.id_album, album.titulo_album, album.ano_lancamento, artista.id_artista, artista.nome, musica.titulo
+      FROM musica 
+      INNER JOIN album ON musica.id_album = album.id_album
+      INNER JOIN artista ON album.id_artista = artista.id_artista
+      WHERE album.id_album = ?
+  `;
+
+  // Artistas do Dropdown
+  const queryArtistas = 'SELECT id_artista, nome FROM artista';
+
+  mysql.query(queryAlbum, [albumId], (error, dadosAlbum) => {
+
+      mysql.query(queryArtistas, (err, todosArtistas) => {
+
+          res.render('editar', { dados: dadosAlbum, todosArtistas });
+      });
   });
 });
+
 
 
 // Rota para processar a submissão do formulário de edição
@@ -146,6 +169,28 @@ app.post('/editar-album/:id', (req, res) => {
     }
   );
 });
+
+/*******
+  DELETE
+********/
+
+// Rota para processar a exclusão da música
+app.post('/excluir-musica/:id', (req, res) => {
+  const musicaId = req.params.id;
+
+  // Exclui a música do banco de dados
+  mysql.query('DELETE FROM musica WHERE id_musica = ?', [musicaId], (error, result) => {
+    if (error) {
+      console.error('Erro ao excluir a música:', error);
+      return res.status(500).send('Erro interno do servidor');
+    }
+
+    // Redireciona para a página principal ou exibe uma mensagem de sucesso
+    res.redirect('/');
+  });
+});
+
+
 
 
 app.listen(port, () => {
